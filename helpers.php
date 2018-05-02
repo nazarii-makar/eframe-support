e8fc60b09ca519436a73141fee55286d26f2a13b
Nazarii_Makar <nazar.makar@magnise.com>
2018-05-02 14:44:28 +0300

    Add currency helper

--- helpers.php
+++ helpers.php
@@ -12,3 +12,32 @@ if ( ! function_exists('route_parameter')) {
         return data_get(app('request')->route(), '2.' . $key, $default);
     }
 }
+
+if ( ! function_exists('currency')) {
+    /**
+     * @param       $amount
+     * @param       $from
+     * @param       $to
+     * @param null  $date
+     * @param array $options
+     *
+     * @return float
+     */
+    function currency($amount, $from, $to, $date = null, $options = [])
+    {
+        $from = strtoupper($from);
+        $to   = strtoupper($to);
+
+        if (null === $date) {
+            $eur_from_rate = \Swap\Laravel\Facades\Swap::latest("EUR/{$from}", $options);
+            $eur_to_rate   = \Swap\Laravel\Facades\Swap::latest("EUR/{$to}", $options);
+        } else {
+            $eur_from_rate = \Swap\Laravel\Facades\Swap::historical("EUR/{$from}", $date, $options);
+            $eur_to_rate   = \Swap\Laravel\Facades\Swap::historical("EUR/{$to}", $date, $options);
+        }
+
+        $to_from_rate = $eur_to_rate->getValue() / $eur_from_rate->getValue();
+
+        return $amount * $to_from_rate;
+    }
+}
